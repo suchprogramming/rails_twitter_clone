bf3d6e39047d2969c522dff2d5297fd2656e8e57
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-03-31 22:42:01 -0700

    add twitterize handle callback and user/tweets relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :tweets, dependent: :destroy
+
+  before_create :twitterize_handle
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+  def twitterize_handle
+    handle.prepend("@")
+  end
 end
